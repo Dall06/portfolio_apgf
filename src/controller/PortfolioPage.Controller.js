80bb4bb7f89dbeee6c27fb029884fb5e2db37d1a
dall06 <devleon.06@gmail.com>
2023-06-07 11:41:29 -0600

    fix: carousel

--- src/controller/PortfolioPage.Controller.js
+++ src/controller/PortfolioPage.Controller.js
@@ -3,35 +3,31 @@ const coffeImagesPath = [
     '../../../../assets/img/img_c_2.png',
     '../../../../assets/img/img_c_3.png',
 ]
+
+var coffeImagesPathIndex = 1; // start with next
+
+function onClickNextCoffe() {
+    if (coffeImagesPathIndex > coffeImagesPath.length - 1) {
+        coffeImagesPathIndex = 0;
+    }
+
+    let image = document.getElementById('img-coffe');
+    image.src = coffeImagesPath[coffeImagesPathIndex++];
+}
+
 const virginiaImagesPath = [
     '../../../../assets/img/img_v_1.png',
     '../../../../assets/img/img_v_2.png',
     '../../../../assets/img/img_v_3.png',
 ]
 
-function onClickNextCoffe() {
-    let image = document.getElementById('img-coffe');
-    // Get the current image source
-    let currentSrc = image.src;
-    // Find the index of the current image path in the list
-    let currentIndex = coffeImagesPath.indexOf(currentSrc);
-    // Calculate the index of the next image in the list
-    let nextIndex = (currentIndex + 1) % coffeImagesPath.length;
-
-    
-}
+var virginiaImagesPathIndex = 1; // start with next
 
 function onClickNextVirginia() {
-    var image = document.getElementById('img-virginia');
-    // Get the current image source
-    var currentSrc = image.src;
-    
-    // Find the index of the current image path in the list
-    var currentIndex = virginiaImagesPath.indexOf(currentSrc);
-    
-    // Calculate the index of the next image in the list
-    var nextIndex = (currentIndex + 1) % virginiaImagesPath.length;
-    
-    // Set the source of the image to the next image path
-    image.src = virginiaImagesPath[nextIndex];
+    if (virginiaImagesPathIndex > virginiaImagesPath.length - 1) {
+        virginiaImagesPathIndex = 0;
+    }
+
+    let image = document.getElementById('img-virginia');
+    image.src = virginiaImagesPath[virginiaImagesPathIndex++];
 }
